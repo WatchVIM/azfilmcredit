62586c04f1457a16e0526cc8d8bc938916008777
WatchVIM <support@watchvim.com>
2026-01-02 19:23:37 -0700

    Update logout.js

--- api/admin/logout.js
+++ api/admin/logout.js
@@ -1,4 +1,4 @@
 export default async function handler(req, res) {
-  res.setHeader("Set-Cookie", [`azfc_admin=; Path=/; HttpOnly; SameSite=Lax; Max-Age=0; Secure`]);
-  res.status(200).json({ success: true });
+  res.setHeader("Set-Cookie", `azfc_admin=; Path=/; HttpOnly; SameSite=Lax; Max-Age=0; Secure`);
+  return res.status(200).json({ success: true });
 }
